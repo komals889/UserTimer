24bdf3f89c471920b63ed2e0229f08c727d54265
komals889 <81618021+komals889@users.noreply.github.com>
2022-09-04 09:34:10 +0530

    timer added

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,15 +2,50 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Index() {
-    const navigate=useNavigate()
-  return (
-    <div className='text-center'> 
-         Welcome To Home Page
+  const navigate=useNavigate()
+  const [seconds,setseconds]=useState(0)
+  const [minute,setminutes]=useState(0)
 
-         <button className='btn btn-dark' onClick={e => {
-                      localStorage.removeItem("signup")
-                      navigate("/")
-                  }}>Sign Out</button>
-    </div>
-  )
+  let timer;
+  useEffect(()=>{
+      timer= setInterval(()=>{
+          setseconds(seconds+1)
+          if(seconds===59){
+              setminutes(minute+1);
+              setseconds(0)
+          }
+      },1000)
+      return ()=> clearInterval(timer)
+  })
+  const restart=()=>{
+      setseconds(0)
+      setminutes(0)
+  }
+  const stop=()=>{
+      clearInterval(timer)
+  }
+return (
+  <div className='text-center container-fluid'> 
+       Welcome To Home Page
+
+          <div className="row">
+              <div className="col-sm-6 offset-sm-3">
+                  <div className="card">
+                      <div className="card-header">
+                          Timer
+                      </div>
+                      <div className="card-body">
+                          <h4>{minute<10?"0"+minute:minute}:{seconds<10?"0"+seconds:seconds}</h4><br />
+                          <button className="btn btn-danger w-50 m-2" onClick={restart}>Restart</button>
+                          <button className="btn btn-info w-50 m-2" onClick={stop}>Stop</button>
+                      </div>
+                  </div><br />
+       <button className='btn btn-dark' onClick={e => {
+                    localStorage.removeItem("signup")
+                    navigate("/")
+                }}>Sign Out</button><br />
+              </div><br />
+          </div> <br />
+  </div>
+)
 }
